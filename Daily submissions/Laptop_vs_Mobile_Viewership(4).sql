3a204435276755f372fce6ced73203452d08955d
ayemaqu <ayemaqu@gmail.com>
2026-05-19 10:37:25 -0400

    I added an extra line to show another way of doing the query

--- Daily submissions/Laptop_vs_Mobile_Viewership(4).sql
+++ Daily submissions/Laptop_vs_Mobile_Viewership(4).sql
@@ -4,3 +4,8 @@
 SELECT COUNT(*) FILTER (WHERE device_type = 'laptop') AS laptop_views,
   COUNT(*) FILTER (WHERE device_type = 'tablet' OR device_type = 'phone') AS mobile_views
 FROM viewership;
+
+
+-- or you can also write it this way
+  COUNT(*) FILTER (WHERE device_type IN ('tablet', 'phone')) AS mobile_views
+
